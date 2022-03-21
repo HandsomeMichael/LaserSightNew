e6905900b2b51cd5d7fb6107566ea23a77838d3a
HandsomeMichael <mikacabi@gmail.com>
2022-03-21 21:06:58 +0800

    v0.6 commit

--- LaserSightNew.cs
+++ LaserSightNew.cs
@@ -1,18 +1,21 @@
 using Microsoft.Xna.Framework; //Vector2
 using Microsoft.Xna.Framework.Graphics; //SpriteBatch | Texture2D
+using Newtonsoft.Json; //JsonConverterAttribute
+using Newtonsoft.Json.Converters; //StringEnumConverter
+using System;// basic stuff like Math class
+using System.Collections.Generic; // List<>
 using System.ComponentModel; //DefaultValueAttribute
+using System.Reflection; // reflection ( no way)
 using Terraria; //Main.LocalPlayer/Main.Player | Item
+using Terraria.DataStructures; // structure
+using Terraria.GameInput; // hotkey
+using Terraria.Graphics; // graphics
+using Terraria.Graphics.Shaders; // shader
+using Terraria.ID; // id
 using Terraria.ModLoader; //Mod
 using Terraria.ModLoader.Config; //ModConfig
 using Terraria.ModLoader.Config.UI; //ModConfigUI
-using System;// basic stuff like Math class
-using System.Collections.Generic; // List<>
-using Terraria.DataStructures; // structure
-using Terraria.Graphics; // graphics
-using Terraria.ID; // id
-using Terraria.GameInput; // hotkey
-using System.Reflection; // reflection ( no way)
-using Terraria.Graphics.Shaders; // shader
+using Terraria.UI; //UIMouseEvent
 
 namespace LaserSightNew
 {
@@ -25,8 +28,17 @@ namespace LaserSightNew
 		public override void Load ()
 		{
             ToggleKey = RegisterHotKey("Quick Toggle Laser", "P");
-			ToggleAimKey = RegisterHotKey("Quick Toggle Aim Laser", "T");
-			AimKey = RegisterHotKey("Quick Aim Laser", "Y");
+			ToggleAimKey = RegisterHotKey("Quick Toggle Lock-On Aiming", "T");
+			AimKey = RegisterHotKey("Quick Lock-On Aiming", "Y");
+		}
+		public override void PostSetupContent() {
+			var obama = ModLoader.GetMod("ObamaCamera");
+			if (obama != null && LaserConfig.get.obamaMod) {
+				// yes this is complex but its better than il edit and reflection, i call this "il edit replacement"
+				obama.Call("typeModifier",(Func<string>)(() => ((LaserToggle.Aim && LaserSight.hitmark != -1) ? "Enemy and Player" : "None") ));
+				obama.Call("loopModifier",(Func<int>)(() => ((LaserToggle.Aim && LaserSight.hitmark != -1) ? LaserSight.hitmark : -2) ),
+				(Func<bool>)(() => ((LaserToggle.Aim && LaserSight.hitmark != -1) ? false : true) ));
+			}
 		}
 		public override void Unload() {
 			AimKey = null;
@@ -51,7 +63,7 @@ namespace LaserSightNew
 			if (LaserSightNew.ToggleAimKey.JustPressed)
 			{
 				AutoAim = !AutoAim;
-				CombatText.NewText(player.getRect(),(AutoAim ? Color.LightGreen : Color.Pink),(AutoAim ? "Aim Lock Enabled" : "Aim Lock Disabled"));
+				CombatText.NewText(player.getRect(),(AutoAim ? Color.LightGreen : Color.Pink),(AutoAim ? "Lock-On Aiming Enabled" : "Lock-On Aiming Disabled"));
 			}
 			if (LaserSightNew.AimKey.Current || AutoAim) {
 				if (LaserSight.hitmark != -1) {
@@ -71,15 +83,6 @@ namespace LaserSightNew
 		//hitmark , the index of targeted enemy asdsdsd
 		public static int hitmark;
 		public override void PostDrawTiles() {
-			if (LaserToggle.Aim) {
-				if (hitmark != -1) {
-					NPC npc = Main.npc[hitmark];
-					if (npc.active) {
-						Main.mouseX = (int)(npc.Center.X + (npc.velocity.X*LaserConfig.get.laserEnemyPredict) - Main.screenPosition.X);
-						Main.mouseY = (int)(npc.Center.Y + (npc.velocity.Y*LaserConfig.get.laserEnemyPredict) - Main.screenPosition.Y);
-					}
-				}
-			}
 			DrawPlayerLaser(Main.LocalPlayer);
 		}
 		public void DrawPlayerLaser (Player player)
@@ -115,11 +118,38 @@ namespace LaserSightNew
 
 					*/
 
-					// color setup
-					Color color = (LaserConfig.get.laserDisco ? Main.DiscoColor : LaserConfig.get.laserColor);
+					// color setup (laserColor = ColorA | markerColor = ColorB)
+					Color colorA = (LaserConfig.get.laserDisco ? Main.DiscoColor : LaserConfig.get.laserColor);
+					//Color colorB = (LaserConfig.get.markerDisco ? Main.DiscoColor : LaserConfig.get.markerColor);
+					
+					//Separate color setup for Target-Marker
+					Color colorB;
+					if (LaserConfig.get.markerColorLaser == false)
+					{
+						if (LaserConfig.get.markerDisco == true && LaserConfig.get.laserDisco == false)
+						{
+							//Applies Disco Effect to Target-Marker
+							colorB = Main.DiscoColor;
+						}
+						else
+						{
+							//Target-Marker will use markerColor
+							colorB = LaserConfig.get.markerColor;
+						}
+					}
+					else
+					{
+						//Target-Marker will use laserColor
+						colorB = colorA;
+					}
 
 					// if npc is gone , set hitmark to -1 
-					if (hitmark > -1 && !Main.npc[hitmark].active) {hitmark = -1;}
+					if (hitmark > -1) {
+						NPC npc = Main.npc[hitmark];
+						if (!npc.active || npc.dontTakeDamage || npc.townNPC) {
+							hitmark = -1;
+						}
+					}
 
 					// only accept valid index
 					if (LaserConfig.get.laserEnemy && hitmark > -1) {
@@ -144,7 +174,9 @@ namespace LaserSightNew
 						}
 
 						//texture
-						var text = ModContent.GetTexture("LaserSightNew/Cross");
+						string path = "LaserSightNew/Texture/"+(LaserConfig.get.smooth ? "Smooth/" : "Rough/");
+						
+						Texture2D texture = ModContent.GetTexture(path+LaserConfig.get.markerTexture);
 
 						//do some sinwave scale crap
 						float scale = (float)Math.Sin(Main.GameUpdateCount/60f);
@@ -152,8 +184,8 @@ namespace LaserSightNew
 						scale += 0.3f;
 
 						//draw the hitmark
-						Main.spriteBatch.Draw(text, npc.Center + (npc.velocity*LaserConfig.get.laserEnemyPredict) - Main.screenPosition, null, color, 
-						MathHelper.ToRadians(Main.GameUpdateCount)*1.5f, text.Size()/2f, scale, SpriteEffects.None, 0);
+						Main.spriteBatch.Draw(texture, npc.Center + (npc.velocity*LaserConfig.get.laserEnemyPredict) - Main.screenPosition, null, colorB, 
+						MathHelper.ToRadians(Main.GameUpdateCount)*1.5f, texture.Size()/2f, scale, SpriteEffects.None, 0);
 
 						//end
 						Main.spriteBatch.End();
@@ -177,45 +209,63 @@ namespace LaserSightNew
 					//setup some variable
 					Vector2 endPoint = player.Center;
 					Vector2 laserStart = player.Center;
+
+					//check where already done in before
+					bool run = true;
+					if (LaserToggle.Aim) {
+						if (hitmark != -1) {
+							run = false;
+							NPC npc = Main.npc[hitmark];
+							Main.mouseX = (int)(npc.Center.X + (npc.velocity.X*LaserConfig.get.laserEnemyPredict) - Main.screenPosition.X);
+							Main.mouseY = (int)(npc.Center.Y + (npc.velocity.Y*LaserConfig.get.laserEnemyPredict) - Main.screenPosition.Y);
+							endPoint = npc.Center + (npc.velocity*LaserConfig.get.laserEnemyPredict);
+						}
+					}
+
 					float LaserRotation = (Main.MouseWorld - laserStart).ToRotation();
 					Vector2 offset = new Vector2(5,5);
 
-					if (LaserConfig.get.laserMouse) {
+					// dont run searching laser end if it auto aim to prevent draw bug
+					if (run) {
+						if (LaserConfig.get.laserMouse) {
 						// this one is a bit offset and i tried doing stuff but it didnt work
-						while (!PointInTile(endPoint) && Vector2.Distance(endPoint,Main.MouseWorld + offset) > 20f){
-							endPoint += Vector2.Normalize((Main.MouseWorld + offset) - endPoint)*8f;
-						}
-					}
-					else {
-						//awawawawawawawawawawawawawa
-						for (int k = 0; k < LaserConfig.get.laserLimit; k++)
-						{
-							Vector2 posCheck = endPoint + Vector2.UnitX.RotatedBy(LaserRotation) * k * 8;
-							if (PointInTile(posCheck) || k == 159)
-							{
-								endPoint = posCheck;
-								break;
+							while (!PointInTile(endPoint) && Vector2.Distance(endPoint,Main.MouseWorld + offset) > 20f){
+								endPoint += Vector2.Normalize((Main.MouseWorld + offset) - endPoint)*8f;
 							}
 						}
-					}
-
-					// this is how the laser stops at npcs, it checks for the closest npc that collide with the laser
-					int b = -1;
-					for (int i = 0; i < Main.maxNPCs; i++)
-					{
-						NPC npc = Main.npc[i];
-						if (npc.active) {
-							float between = Vector2.Distance(npc.Center, laserStart);
-							bool closest = Vector2.Distance(laserStart, endPoint) > between;
-							var targetHitbox = npc.Hitbox;
-							float point = 0f;
-							// let collison do all the magic , honestly i dont even know how does this work without destroying frame rate
-							bool collide = Collision.CheckAABBvLineCollision(targetHitbox.TopLeft(), targetHitbox.Size(), laserStart,endPoint, 5, ref point);
-							if ((closest || b == -1 ) && collide) {
-								b = i;
-								endPoint = npc.Center;
-								if (!LaserToggle.Aim) {
-									hitmark = i;
+						else {
+							//awawawawawawawawawawawawawa
+							for (int k = 0; k < LaserConfig.get.laserLimit; k++)
+							{
+								Vector2 posCheck = endPoint + Vector2.UnitX.RotatedBy(LaserRotation) * k * 8;
+								if (PointInTile(posCheck) || k == 159)
+								{
+									endPoint = posCheck;
+									break;
+								}
+							}
+						}
+						// this is how the laser stops at npcs, it checks for the closest npc that collide with the laser
+						if (LaserConfig.get.laserCollision == "NPCs & Tiles" || LaserConfig.get.laserEnemy)
+						{
+							int b = -1;
+							for (int i = 0; i < Main.maxNPCs; i++)
+							{
+								NPC npc = Main.npc[i];
+								if (npc.active && !npc.townNPC && !npc.friendly) {
+									float between = Vector2.Distance(npc.Center, laserStart);
+									bool closest = Vector2.Distance(laserStart, endPoint) > between;
+									var targetHitbox = npc.Hitbox;
+									float point = 0f;
+									// let collison do all the magic , honestly i dont even know how does this work without destroying frame rate
+									bool collide = Collision.CheckAABBvLineCollision(targetHitbox.TopLeft(), targetHitbox.Size(), laserStart,endPoint, 5, ref point);
+									if ((closest || b == -1 ) && collide) {
+										b = i;
+										if (LaserConfig.get.laserCollision == "NPCs & Tiles") {endPoint = npc.Center;}
+										if (LaserConfig.get.laserEnemy) {
+											hitmark = i;
+										}
+									}
 								}
 							}
 						}
@@ -231,12 +281,12 @@ namespace LaserSightNew
 					var target = new Rectangle((int)pos.X, (int)pos.Y, width, (int)(height * 1.2f));
 
 					// draw with rectangle and rotation
-					Main.spriteBatch.Draw(texBeam, target, null, color, LaserRotation, origin, 0, 0);
+					Main.spriteBatch.Draw(texBeam, target, null, colorA, LaserRotation, origin, 0, 0);
 
 					//lighting
 					if (LaserConfig.get.laserLight) {
 						for (int i = 0; i < width; i += 10) {
-							Lighting.AddLight(pos + Vector2.UnitX.RotatedBy(LaserRotation) * i + Main.screenPosition, color.ToVector3() * height * 0.030f);         
+							Lighting.AddLight(pos + Vector2.UnitX.RotatedBy(LaserRotation) * i + Main.screenPosition, colorA.ToVector3() * height * 0.030f);         
 						}
 					}
 
@@ -258,6 +308,8 @@ namespace LaserSightNew
 		// check if the point is colliding a tile
 		public static bool PointInTile(Vector2 point)
         {
+			// no collison , awa
+			if (LaserConfig.get.laserCollision == "No Collision") {return false;}
             Point16 startCoords = new Point16((int)point.X / 16, (int)point.Y / 16);
             for(int x = -1; x <= 1; x++)
                 for(int y = -1; y <= 1; y++)
@@ -297,76 +349,68 @@ namespace LaserSightNew
 	[Label("Configs")]
 	public class LaserConfig : ModConfig
 	{
-		// save the config
-		public static void SaveConfig(){
-			typeof(ConfigManager).GetMethod("Save", BindingFlags.Static | BindingFlags.NonPublic).Invoke(null, new object[1] { get });
-		}
+		//Saves the config
+		public static void SaveConfig ()
+		{typeof(ConfigManager).GetMethod("Save", BindingFlags.Static | BindingFlags.NonPublic).Invoke(null, new object[1] { get });}
 
 		public override ConfigScope Mode => ConfigScope.ClientSide;
 		public static LaserConfig get => ModContent.GetInstance<LaserConfig>();
 		
-		[Header("Visuals")]
+		[Header("Laser Settings")]
 		
 		[Label("Laser Enabled")]
 		[Tooltip("Whether a laser appears or not")]
 		[DefaultValue(true)]
 		public bool laserEnabled;
-
-		[Label("Laser Distance Limit to mouse")]
-		[Tooltip("Limit laser length to mouse\nthis will ignore 'Laser Distance Limit'")]
+		
+		[Label("Show laser in any weapons or unarmed")]
+		[Tooltip("Allows laser to show in any weapon types, includes unarmed")]
+		[DefaultValue(false)]
+		public bool anyWeap;
+		
+		[Label("Laser-only with scope")]
+		[Tooltip("Activate laser only when using a sniper scope")]
+		[DefaultValue(false)]
+		public bool laserScope;
+		
+		[Label("Laser Collision Type")]
+		[Tooltip("Change how laser works with NPCs and Tiles")]
+		[OptionStrings(new string[] { "NPCs & Tiles", "Tiles Only", "No Collision"})]
+		[DefaultValue("NPCs & Tiles")]
+		[DrawTicks]
+		public string laserCollision;
+		
+		[Label("Limit laser distance to mouse cursor")]
+		[Tooltip("Limit laser length to mouse, and ignores 'Laser Distance Limit'")]
 		[DefaultValue(false)]
 		public bool laserMouse;
-
+		
 		[Label("Laser Distance Limit")]
 		[Tooltip("The distance limit of the laser")]
-		[Range(30, 500)]
+		[Range(30, 1000)]
 		[Increment(10)]
-		[DefaultValue(200)]
+		[DefaultValue(500)]
 		[Slider] 
 		public int laserLimit;
-
+		
+		[Label("Laser Auto-Scale")]
+		[Tooltip("Adjust laser width based on camera zoom, and ignores 'Laser Auto-Scale'")]
+		[DefaultValue(true)]
+		public bool scaleAuto;
+		
 		[Label("Laser Scale")]
-		[Tooltip("Change how thin or w i d e the laser could get")]
+		[Tooltip("Change how thin or WIDE the laser could get")]
 		[Range(1, 20)]
 		[Increment(1)]
 		[DefaultValue(10)]
 		[Slider] 
 		public int scale;
-
-		[Label("Laser Auto Scale")]
-		[Tooltip("Laser width will be based on your camera zoom\n this will ignore 'Laser Scale'")]
-		[DefaultValue(false)]
-		public bool scaleAuto;
-
-		[Label("Laser Enemy Indicator")]
-		[Tooltip("Show a mark if the laser detect an enemy\nuse one of the hotkey to aim at the enemy at all time")]
-		[DefaultValue(true)]
-		public bool laserEnemy;
-
-
-		[Label("Laser Aim Prediction")]
-		[Tooltip("The intensity of aim lock predict enemy movement\nmay not work with some enemy that has custom movement")]
-		[Range(0, 10)]
-		[Increment(1)]
-		[DefaultValue(0)]
-		[Slider] 
-		public int laserEnemyPredict;
-
+		
 		[Label("Laser Light")]
 		[Tooltip("Make laser emmit light")]
 		[DefaultValue(true)]
 		public bool laserLight;
-
-		[Label("Show laser in any weapons or unarmed")]
-		[Tooltip("Allows laser to be shown in any weapons other than ranged weapon, including unarmed")]
-		[DefaultValue(false)]
-		public bool anyWeap;
-
-		[Label("Show laser in only when using sniper scope")]
-		[Tooltip("Show laser only when player using a sniper scope")]
-		[DefaultValue(false)]
-		public bool laserScope;
-
+		
 		[Label("Laser Dye")]
 		[Tooltip("The laser custom dye usage\nMust be a valid dye !")]
 		public ItemDefinition laserDye = new ItemDefinition("Terraria GoldOre");
@@ -376,10 +420,159 @@ namespace LaserSightNew
 		[DefaultValue(typeof(Color), "255,100,0,255")]
 		public Color laserColor = new Color(255,100,0,255);
 
-		[Label("Laser Disco Color")]
-		[Tooltip("Cycle between colors \ndoes not include amongus color")]
+		[Label("Laser Disco Effect")]
+		[Tooltip("Cycle between colors\nDoes not include amongus color")]
 		[DefaultValue(false)]
 		public bool laserDisco;
+		
+		
+		[Header("Target-Mark Settings")]
+		
+		[Label("Target-Marking Enabled")]
+		//Reserved	//[Tooltip("Mark targets if a laser/mouse points to an enemy\nCan be used along with Lock-On Aiming in the hotkey configs.")]
+		[Tooltip("Mark targets if a laser points to an enemy\nCan be used along with Lock-On Aiming in the hotkey configs.")]
+		[DefaultValue(true)]
+		public bool laserEnemy;
+		
+		//Reserved
+		/*[Label("Target-Marking with Mouse")]
+		[Tooltip("Marks target with mouse cursor instead of laser")]
+		[DefaultValue(false)]
+		public bool markTargetMouse;*/
+
+		[Label("Lock-On Predicted-Aim Accuracy")]
+		[Tooltip("The intensity of aim lock predict enemy movement\nMay not work against enemies with custom movement")]
+		[Range(0, 10)]
+		[Increment(1)]
+		[DefaultValue(0)]
+		[Slider] 
+		public int laserEnemyPredict;
+		
+		[Label("Share color with laser")]
+		[Tooltip("Uses laser color instead of using separate color")]
+		[DefaultValue(true)]
+		public bool markerColorLaser;
+		
+		[Label("Target-Marker Color")]
+		[Tooltip("The separate color for the target-marker")]
+		[DefaultValue(typeof(Color), "255,100,0,255")]
+		public Color markerColor = new Color(255,100,0,255);
+		
+		[Label("Target-Marker Disco Effect")]
+		[Tooltip("Separated disco effect for target-marker\nOnly works if both 'laser color sharing' and 'laser disco effect' were disabled")]
+		[DefaultValue(false)]
+		public bool markerDisco;
+		
+		[Label("Target-Marker Texture")]
+		[DefaultValue(TextureEnum.Marker12)]
+		[Tooltip("Texture for Target-Marker\n(Default: Marker12)")]
+		public TextureEnum markerTexture;
+		
+		[Label("Smooth Texture")]
+		[Tooltip("Make the target-marker uses smooth texture")]
+		[DefaultValue(false)]
+		public bool smooth;
+
+		[Label("Obama Camera Overhaul Cross Mod")]
+		[Tooltip("Make targeted enemy slightly focused\nrequires a reload to change\nrequires obama camera overhaul mod v0.8.7 or higher")]
+		[DefaultValue(false)]
+		public bool obamaMod;
+		
+		//Reserved
+		/*[Label("Target-Marker Animate: Spin")]
+		[Tooltip("Allow the marker to spin")]
+		[DefaultValue(false)]
+		public bool markerSpin;
+		
+		[Label("Target-Marker Animate: Pulse")]
+		[Tooltip("Allow the marker to pulse")]
+		[DefaultValue(false)]
+		public bool markerPulse;*/
+		
+		//Auto-Aim blacklist
+		//Critter
+		//TownNPC
+		//Target Dummy
+		
+		//Custom Target-Marker Texture
+		[JsonConverter(typeof(StringEnumConverter))]
+		[CustomModConfigItem(typeof(TextureElement))]
+		
+		public enum TextureEnum
+		{
+			Marker01,
+			Marker02,
+			Marker03,
+			Marker04,
+			Marker05,
+			Marker06,
+			Marker07,
+			Marker08,
+			Marker09,
+			Marker10,
+			Marker11,
+			Marker12
+		}
+		
+		internal class TextureElement : ConfigElement
+		{
+			Texture2D markerTexture;
+			string[] valueStrings;
+			
+			public override void OnBind ()
+			{
+				base.OnBind ();
+				markerTexture = Terraria.Graphics.TextureManager.Load("Images/UI/Settings_Toggle");
+				valueStrings = Enum.GetNames (memberInfo.Type);
+				TextDisplayFunction = () => memberInfo.Name + ": " + GetStringValue ();
+				if (labelAttribute != null)
+				{
+					TextDisplayFunction = () => labelAttribute.Label + ": " + GetStringValue ();
+				}
+			}
+			
+			void SetValue(TextureEnum value) => SetObject(value);
+
+			TextureEnum GetValue() => (TextureEnum)GetObject();
+			
+			string GetStringValue()
+			{
+				return valueStrings[(int)GetValue()].Replace("_"," ");
+			}
+			
+			public override void Click(UIMouseEvent evt)
+			{
+				base.Click(evt);
+				SetValue(GetValue().NextEnum());
+			}
+
+			public override void RightClick(UIMouseEvent evt)
+			{
+				base.RightClick(evt);
+				SetValue(GetValue().PreviousEnum());
+			}
+			
+			static float timer;
+			
+			public override void Draw(SpriteBatch spriteBatch)
+			{
+				base.Draw(spriteBatch);
+				timer += 0.5f;
+				
+				//reset at 696969f
+				if (timer >= 696969f)
+				{timer = 0;}
+			
+				string path = "LaserSightNew/Texture/"+(LaserConfig.get.smooth ? "Smooth/" : "Rough/");
+				path += GetStringValue().Replace(" ","_");
+				int frame = (int)timer % 25;
+				frame /= 1;
+				CalculatedStyle dimensions = base.GetDimensions();
+				Texture2D texture = ModContent.GetTexture(path);
+				Vector2 pos = new Vector2(dimensions.X + dimensions.Width  -36,dimensions.Y + 15);
+				spriteBatch.Draw(texture, pos, null, Color.White, 0f, texture.Size()/2f, 0.7f, SpriteEffects.None, 0);
+			}
+		}
 	}
 	
 	/*public class hitmarkerColorData
